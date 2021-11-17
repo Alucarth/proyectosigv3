405cf6346b57547548cc08a6aa7cbb8fcd162580
kriskor <1criscor@gmail.com>
2021-11-17 14:00:23 -0400

    Se modifico welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -62,7 +62,7 @@
                                 class="btn btn-primary py-3 px-4 w-full xl:w-32 xl:mr-3 align-top">Iniciar</button>
                         </div>
                     </form>
-
+     
                     
                 </div>
             </div>

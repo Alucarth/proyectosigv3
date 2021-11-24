ff10f2a6d1e0d3c37fe9317a4d4226e69b065997
kriskor <1criscor@gmail.com>
2021-11-24 17:10:49 -0400

    primera modificacion de entidades

--- resources/views/livewire/form-institution.blade.php
+++ resources/views/livewire/form-institution.blade.php
@@ -1,80 +1,102 @@
-<div class="box py-10 px-8 sm:py-20 mt-5">
-    <h2 class="text-lg uppercase text-gray-700 py-4 text-center">{{ $institution->razon_social }}</h2>
-    @include('layout.partials.errors')
+<div class="box py-2 px-8 sm:py-1">
+    <h2 class="text-lg uppercase text-gray-700 text-center font-medium">EMPRESA</h2>
+    <h2 class="text-lg uppercase text-gray-700 text-center">{{ $institution->razon_social }}</h2>
+    <!-- @include('layout.partials.errors') -->
     @include('layout.partials.flashMessage')
 
 
     <div class="intro-y col-span-12 md:col-span-6">
-                <div class="box">
-                    <div class="flex flex-col lg:flex-row items-center p-5 border-b border-gray-200 dark:border-dark-5">
-                        <div class="w-24 h-24 lg:w-12 lg:h-12 image-fit lg:mr-1">
-                            <img alt="Rubick Tailwind HTML Admin Template" class="rounded-full" src="">
-                        </div>
-                        <div class="lg:ml-2 lg:mr-auto text-center lg:text-left mt-3 lg:mt-0">
-                            <a href="" class="font-medium">222222</a>
-                            <div class="text-gray-600 text-xs mt-0.5">xxxxx</div>
-                        </div>                        
-                    </div>
-                    <div class="flex flex-wrap lg:flex-nowrap items-center justify-center p-5">
-                        <button class="btn btn-primary py-1 px-2 mr-2">Message</button>
-                        <button class="btn btn-outline-secondary py-1 px-2">Profile</button>
-                    </div>
+        <div class="box">
+            <div class="flex flex-col lg:flex-row items-center p-5 border-b border-gray-200 dark:border-dark-5">
+                <div class="w-24 h-24 lg:w-12 lg:h-12 image-fit lg:mr-1">                            
+                    <span style="font-size: 3em; color: #FAC428;">
+                        <i class="fas fa-edit"></i>
+                    </span>
                 </div>
+                <div class="lg:ml-2 lg:mr-auto text-center lg:text-left mt-3 lg:mt-0">
+                <div class="font-medium text-base">Datos Generales</div>
+                <div class="text-gray-600">Complete la información solicitada</div>
+                </div>                        
             </div>
+            
+            <div class="text-center lg:text-left p-5">
+                <form wire:submit.prevent='updateInstitution' enctype="multipart/form-data" class="grid grid-cols-12 gap-2 items-center">
+                    <div class="col-span-12 sm:col-span-4">
+                        <label class="form-label">NIT</label>
+                        <input wire:model='archivoNit' type="file" class="form-control">
+                        @error('archivoNit') <small class="intro-x sm:ml-auto mt-1 sm:mt-0 text-theme-6 block ">{{ $message }}</small> @enderror
+                    </div>
+                    <div class="col-span-12 sm:col-span-4">
+                        <label class="form-label">Gran Actividad</label>
+                        <input wire:model='rubro' type="text" class="form-control" placeholder="Según el NIT">
+                        @error('rubro') <small class="intro-x sm:ml-auto mt-1 sm:mt-0 text-theme-6 block ">{{ $message }}</small> @enderror
+                    </div>
+                    <div class="col-span-12 sm:col-span-4">
+                        <label class="form-label">Actividad Principal</label>
+                        <input wire:model='actividad' type="text" class="form-control" placeholder="Según el NIT">
+                        @error('actividad') <small class="intro-x sm:ml-auto mt-1 sm:mt-0 text-theme-6 block ">{{ $message }}</small> @enderror
+                    </div>
+                    <div class="col-span-12 sm:col-span-12 flex flex-wrap lg:flex-nowrap items-center justify-center p-5">
+                        <button type="submit" class="btn btn-sm btn-primary">Guardar</button>
+                    </div>                            
+                </form>
+            </div>                                    
+        </div>
+    </div>
 
 
 
-
-
-
-
-    <h2 class="text-lg uppercase text-gray-900 py-4 text-left">Datos: </h2>
-    <form wire:submit.prevent='updateInstitution' enctype="multipart/form-data"
-        class="grid grid-cols-12 gap-2 items-center">
-        <div class="col-span-12 sm:col-span-3">
-            <label class="form-label">NIT</label>
-            <input wire:model='archivoNit' type="file" class="form-control">
+    <div class="intro-y col-span-12 md:col-span-6">
+        <div class="box">
+            <div class="flex flex-col lg:flex-row items-center p-5 border-b border-gray-200 dark:border-dark-5">
+                <div class="w-24 h-24 lg:w-12 lg:h-12 image-fit lg:mr-1">                            
+                    <span style="font-size: 3em; color: #FAC428;">
+                        <i class="fas fa-user"></i>
+                    </span>
+                </div>
+                <div class="lg:ml-2 lg:mr-auto text-center lg:text-left mt-3 lg:mt-0">
+                <div class="font-medium text-base">Representante Legal</div>
+                <div class="text-gray-600">Complete la información solicitada</div>
+                </div>                        
+            </div>
+            
+            <div class="text-center lg:text-left p-5">
+            <form wire:submit.prevent='updateLegalRepresentative' enctype="multipart/form-data"
+                    class="grid grid-cols-12 gap-2 items-center">
+                    <div class="col-span-12 sm:col-span-4">
+                        <label class="form-label">Nombres</label>
+                        <input wire:model='nombreRepresentante' type="text" class="form-control" placeholder="">
+                        @error('nombreRepresentante') <small class="intro-x sm:ml-auto mt-1 sm:mt-0 text-theme-6 block ">{{ $message }}</small> @enderror
+                    </div>
+                    <div class="col-span-12 sm:col-span-4">
+                        <label class="form-label">Apellido Paterno</label>
+                        <input wire:model='paternoRepresentante' type="text" class="form-control" placeholder="">
+                        @error('paternoRepresentante') <small class="intro-x sm:ml-auto mt-1 sm:mt-0 text-theme-6 block ">{{ $message }}</small> @enderror
+                    </div>
+                    <div class="col-span-12 sm:col-span-4">
+                        <label class="form-label">Apellido Materno</label>
+                        <input wire:model='maternoRepresentante' type="text" class="form-control" placeholder="">
+                        @error('maternoRepresentante') <small class="intro-x sm:ml-auto mt-1 sm:mt-0 text-theme-6 block ">{{ $message }}</small> @enderror
+                    </div>
+                    <div class="col-span-12 sm:col-span-6">
+                        <label class="form-label">Teléfono / Celular</label>
+                        <input wire:model='telefonoRepresentante' type="text" class="form-control" placeholder="">
+                        @error('telefonoRepresentante') <small class="intro-x sm:ml-auto mt-1 sm:mt-0 text-theme-6 block ">{{ $message }}</small> @enderror
+                    </div>
+                    <div class="col-span-12 sm:col-span-6">
+                        <label class="form-label">Correo</label>
+                        <input wire:model='emailRepresentante' type="text" class="form-control" placeholder="">
+                        @error('emailRepresentante') <small class="intro-x sm:ml-auto mt-1 sm:mt-0 text-theme-6 block ">{{ $message }}</small> @enderror
+                    </div>
+                    <div class="col-span-12 sm:col-span-12 flex flex-wrap lg:flex-nowrap items-center justify-center p-5">
+                        <button type="submit" class="btn btn-sm btn-primary">Guardar</button>
+                    </div>
+                </form>
+            </div>                                    
         </div>
-        <div class="col-span-12 sm:col-span-3">
-            <label class="form-label">Gran Actividad</label>
-            <input wire:model='rubro' type="text" class="form-control" placeholder="Según el NIT">
-        </div>
-        <div class="col-span-12 sm:col-span-3">
-            <label class="form-label">Actividad Principal</label>
-            <input wire:model='actividad' type="text" class="form-control"
-                placeholder="Según el NIT">
-        </div>
-        <div class="col-span-12 sm:col-span-3 pt-6">
-            <button type="submit" class="btn btn-secondary">Guardar</button>
-        </div>
-    </form>
-    <h2 class="text-lg uppercase text-gray-900 py-4 text-left">Representante Legal: </h2>
-    <form wire:submit.prevent='updateLegalRepresentative' enctype="multipart/form-data"
-        class="grid grid-cols-12 gap-2 items-center">
-        <div class="col-span-12 sm:col-span-3">
-            <label class="form-label">Nombres</label>
-            <input wire:model='nombreRepresentante' type="text" class="form-control" placeholder="Jose Luis">
-        </div>
-        <div class="col-span-12 sm:col-span-3">
-            <label class="form-label">Apellido Paterno</label>
-            <input wire:model='paternoRepresentante' type="text" class="form-control" placeholder="Delgado">
-        </div>
-        <div class="col-span-12 sm:col-span-3">
-            <label class="form-label">Apellido Materno</label>
-            <input wire:model='maternoRepresentante' type="text" class="form-control" placeholder="Mamani">
-        </div>
-        <div class="col-span-12 sm:col-span-3">
-            <label class="form-label">Teléfono / Celular</label>
-            <input wire:model='telefonoRepresentante' type="text" class="form-control" placeholder="2212584">
-        </div>
-        <div class="col-span-12 sm:col-span-3">
-            <label class="form-label">Correo</label>
-            <input wire:model='emailRepresentante' type="text" class="form-control" placeholder="micorreo@gmail.com">
-        </div>
-        <div class="col-span-12 sm:col-span-3 pt-6">
-            <button type="submit" class="btn btn-secondary">Guardar</button>
-        </div>
-    </form>
+    </div>
+    
+    
 
     <h2 class="text-lg uppercase text-gray-900 py-4 text-left mt-4">Registro de Casa Matriz y Sucursales: </h2>
     <form wire:submit.prevent='addBranch' class="grid grid-cols-12 gap-2 items-center">
@@ -186,6 +208,5 @@
         {{ $coordinators->links() }}
     </div>
 
-    <a class="btn btn-outline-primary py-3 px-4 xl:w-80 mt-3 xl:mt-2 align-left"
-                            href="{{ route('page.dashboard') }}">Finalizar</a>
+    <a class="btn btn-outline-primary py-3 px-4 xl:w-80 mt-3 xl:mt-2 align-left" href="{{ route('page.dashboard') }}">Finalizar</a>
 </div>

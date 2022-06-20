f739e107cc8aa2ca9f548834c1247a5ccc900e33
alucarth <ltorrezs2008@gmail.com>
2022-06-20 16:01:42 -0400

    buscador terminado

--- app/Http/Controllers/ReplacementInstitutionController.php
+++ app/Http/Controllers/ReplacementInstitutionController.php
@@ -16,17 +16,44 @@ class ReplacementInstitutionController extends Controller
     {
         $conditions = array();
         $ci = request('ci')??'';
+        $nombres = request('nombres')??'';
+        $razon_social = request('razon_social')??'';
+
         Log::info($ci);
+
+        $has_search =false;
 
         if(isset($ci) && $ci !==  '')
         {
             array_push($conditions,['ci','like',"%{$ci}%"]);
+            $has_search = true;
         }
-        $repositions = DB::table('people_replacement')
-                ->where($conditions)
-                // ->where('ci','like',$this->ci)
-                // ->orderBy('nro_pago')
-                ->get();
+
+        if(isset($nombres) && $nombres !== '')
+        {
+            $nombres =  mb_strtoupper($nombres, 'UTF-8');
+            array_push($conditions,['nombres','like',"%{$nombres}%"]);
+            $has_search = true;
+        }
+
+        if(isset($razon_social) && $razon_social !== '')
+        {
+            $razon_social =  mb_strtoupper($razon_social, 'UTF-8');
+            array_push($conditions,['razon_social','like',"%{$razon_social}%"]);
+            $has_search = true;
+        }
+
+
+        if($has_search)
+        {
+            $repositions = DB::table('people_replacement')
+                    ->where($conditions)
+                    // ->where('ci','like',$this->ci)
+                    // ->orderBy('nro_pago')
+                    ->get();
+        }else{
+            $repositions = [];
+        }
 
         return response()->json(compact('repositions'));
     }
--- resources/views/pages/replacementList.blade.php
+++ resources/views/pages/replacementList.blade.php
@@ -27,21 +27,22 @@
 
                                     <div class="sm:grid grid-cols-3 gap-2">
                                         <div class="input-group">
-                                            <div id="input-group-3" class="input-group-text">CI</div>
-                                            <input type="text" class="form-control" placeholder="CI Beneficiario"   aria-describedby="input-group-3">
+                                            <div id="input-group-3" class="input-group-text"  >CI</div>
+
+                                            <input type="text" class="form-control" placeholder="CI Beneficiario" data-bind="textInput: ci, event:{ keyup: buscarBeneficiario}"  aria-describedby="input-group-3">
                                         </div>
                                         <div class="input-group mt-2 sm:mt-0">
                                             <div id="input-group-4" class="input-group-text">Nombre</div>
-                                            <input type="text" class="form-control" placeholder="Nombre Beneficiario"  aria-describedby="input-group-4">
+                                            <input type="text" class="form-control" placeholder="Nombre Beneficiario" data-bind="textInput: nombres, event:{ keyup: buscarBeneficiario}" aria-describedby="input-group-4">
                                         </div>
                                         <div class="input-group mt-2 sm:mt-0">
                                             <div id="input-group-5" class="input-group-text">Empresa</div>
-                                            <input type="text" class="form-control" placeholder="Empresa"   aria-describedby="input-group-5">
+                                            <input type="text" class="form-control" placeholder="Empresa"  data-bind="textInput: razon_social, event:{ keyup: buscarBeneficiario}" aria-describedby="input-group-5">
                                         </div>
-                                        <button type="button" class="btn btn-primary shadow-md " data-bind="click: buscarBeneficiario()"  >Buscar</button>
+                                        <button type="button" class="btn btn-primary shadow-md " data-bind="click: clickBuscarBeneficiario"  >Buscar</button>
                                         {{-- <button class="btn btn-primary shadow-md "  wire:click="buscar()"> <i class="fa fa-search mr-2 w-32  "></i> Buscar</button> --}}
                                     </div>
-
+                                    {{-- <pre data-bind="text: ko.toJSON(ci, null, 2)"></pre> --}}
 
                                     <div class="hidden md:block mx-auto text-slate-500"></div>
                                     <div class="w-full sm:w-auto mt-3 sm:mt-0 sm:ml-auto md:ml-0">
@@ -60,29 +61,56 @@
                                                 <th class="whitespace-nowrap">Nro Pago</th>
                                                 <th class="whitespace-nowrap">CI</th>
                                                 <th class="whitespace-nowrap">Nombre</th>
+                                                <th class="whitespace-nowrap">Empresa</th>
                                                 <th class="whitespace-nowrap">Periodo</th>
                                                 <th class="whitespace-nowrap">Monto</th>
                                                 <th class="whitespace-nowrap">Descuentos/Bonos </th>
                                                 <th class="whitespace-nowrap">Salario Basico </th>
                                                 <th class="whitespace-nowrap">Oficial Operativo </th>
+                                                <th class="whitespace-nowrap">Total Ganado </th>
                                             </tr>
                                         </thead>
                                         <tbody data-bind="foreach: reposiciones">
 
-                                            <tr >
+                                            <tr  >
                                                 {{-- <td> 1 </td> --}}
                                                 <td data-bind="text: nro_pago"> </td>
                                                 <td data-bind="text: ci"> </td>
                                                 <td data-bind="text: nombres"> </td>
-                                                <td> </td>
-                                                <td> </td>
-                                                <td> </td>
-                                                <td> </td>
-                                                <td> </td>
+                                                <td data-bind="text: razon_social"> </td>
+                                                <td data-bind="text: fecha_periodo"> </td>
+                                                <td data-bind="text: monto"> </td>
+                                                <td data-bind="text: descuentos_bonos"> </td>
+                                                <td data-bind="text: salario_basico"> </td>
+                                                <td data-bind="text: email"> </td>
+                                                <td data-bind="text: total_ganado"> </td>
                                             </tr>
+
 
                                         </tbody>
                                     </table>
+                                    <table class="table">
+
+                                        <tbody>
+                                            <tr class="bg-gray-700 dark:bg-dark-1 text-white" >
+                                                <td>&nbsp;</td>
+                                                <td>&nbsp;</td>
+                                                <td>&nbsp;</td>
+                                                <td>&nbsp;</td>
+                                                <td>&nbsp;</td>
+
+                                                <td>&nbsp;</td>
+                                                <td>&nbsp;</td>
+                                                <td>&nbsp;</td>
+                                                <td>&nbsp;</td>
+                                                <td>&nbsp;</td>
+                                                <td>Total <strong data-bind="text: total_repo"></strong></td>
+                                            </tr>
+
+
+                                        </tbody>
+                                    </table>
+
                                 </div>
                             </div>
 
@@ -100,28 +128,91 @@
 <script>
     console.log("haber si funciona")
     var viewModel = {
-        ci: ko.observable(),
-        beneficiario: ko.observable(),
+        ci: ko.observable(""),
+        nombres: ko.observable(""),
+        razon_social: ko.observable(""),
         reposiciones: ko.observableArray([]),
-        buscarBeneficiario: function(){
-            axios.get('/repositions')
-            .then(function (response) {
-                // handle success
-                console.log(response.data.repositions)
-                viewModel.reposiciones(response.data.repositions)
-                // response.data.repositions.forEach(item => {
-                //     viewModel.reposiciones.push(item)
+        total_repo: ko.observable(""),
+        buscarBeneficiario: function(data,event)
+        {
+            if(event.keyCode == 13 )
+            {
+                console.log('presionando enter')
+                let params =  {
+                    params: {
+                        ci: viewModel.ci(),
+                        nombres: viewModel.nombres(),
+                        razon_social: viewModel.razon_social()
+                    }
+                }
+                console.log(params)
+                axios.get('/repositions', params)
+                .then(function (response) {
+                    // handle success
+                    console.log(response.data.repositions)
+                    viewModel.reposiciones(response.data.repositions)
 
-                // });
-                console.log(viewModel.reposiciones());
-            })
-            .catch(function (error) {
-                // handle error
-                console.log(error);
-            })
-            .then(function () {
-                // always executed
-            });
+                    var total = 0;
+                    viewModel.reposiciones().forEach(item => {
+                        total += parseFloat(item.total_ganado)
+                    });
+
+                    viewModel.total_repo(total)
+                    console.log(viewModel.total_repo())
+                    // response.data.repositions.forEach(item => {
+                    //     viewModel.reposiciones.push(item)
+
+                    // });
+                    console.log(viewModel.reposiciones());
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                })
+                .then(function () {
+                    // always executed
+                });
+            }
+        },
+        clickBuscarBeneficiario()
+        {
+
+                console.log('usando buscador generico')
+                let params =  {
+                    params: {
+                        ci: viewModel.ci(),
+                        nombres: viewModel.nombres(),
+                        razon_social: viewModel.razon_social()
+                    }
+                }
+                console.log(params)
+                axios.get('/repositions', params)
+                .then(function (response) {
+                    // handle success
+                    console.log(response.data.repositions)
+                    viewModel.reposiciones(response.data.repositions)
+
+                    var total = 0;
+                    viewModel.reposiciones().forEach(item => {
+                        total += parseFloat(item.total_ganado)
+                    });
+
+                    viewModel.total_repo(total)
+                    // response.data.repositions.forEach(item => {
+                    //     viewModel.reposiciones.push(item)
+
+                    // });
+                    console.log(viewModel.reposiciones());
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                })
+                .then(function () {
+                    // always executed
+                });
+
+
         }
     }
     ko.applyBindings(viewModel);

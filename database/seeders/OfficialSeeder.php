891369fb00d372dd95bbb2b63a2c39db0efe8639
alucarth <ltorrezs2008@gmail.com>
2022-07-05 09:59:15 -0400

    creacion de usuarios oficiales

--- database/seeders/OfficialSeeder.php
+++ database/seeders/OfficialSeeder.php
@@ -15,13 +15,13 @@ class OfficialSeeder extends Seeder
      * @return void
      */
     public function run()
-    {      
+    {
 
-        $user = new User();        
+        $user = new User();
         $user->email = "admin@sigv3.com";
         $user->codigo = Str::uuid()->toString();
         $user->activation = 1;
-        $user->password = bcrypt("515t3m45");        
+        $user->password = bcrypt("515t3m45");
         $user->save();
 
         $user->assignRole('admin');
@@ -32,14 +32,14 @@ class OfficialSeeder extends Seeder
         $official->materno = "ADMIN";
         $official->user_id = $user->id;
         $official->save();
-        
 
 
-        $user = new User();        
+
+        $user = new User();
         $user->email = "oficial@sigv3.com";
         $user->codigo = Str::uuid()->toString();
         $user->activation = 1;
-        $user->password = bcrypt("0f1c14l");        
+        $user->password = bcrypt("0f1c14l");
         $user->save();
 
         $user->assignRole('oficial');
@@ -52,13 +52,79 @@ class OfficialSeeder extends Seeder
         $official->save();
 
 
+        $user = new User();
+        $user->email = "blanca.aruquipa@planificacion.gob.bo";
+        $user->codigo = Str::uuid()->toString();
+        $user->activation = 1;
+        $user->password = bcrypt("0f1c14l");
+        $user->save();
+
+        $user->assignRole('oficial');
+
+        $official = new Official();
+        $official->nombres = "BLANCA HELEN";
+        $official->paterno = "ARUQUIPA";
+        $official->materno = "MAYTA";
+        $official->user_id = $user->id;
+        $official->save();
+
+        $user = new User();
+        $user->email = "denny.munoz@planificacion.gob.bo";
+        $user->codigo = Str::uuid()->toString();
+        $user->activation = 1;
+        $user->password = bcrypt("0f1c14l");
+        $user->save();
+
+        $user->assignRole('oficial');
+
+        $official = new Official();
+        $official->nombres = "DENNY JUCELIA";
+        $official->paterno = "MUÑOZ";
+        $official->materno = "ANTEQUERA";
+        $official->user_id = $user->id;
+        $official->save();
+
+        $user = new User();
+        $user->email = "milenka.acarapi@planificacion.gob.bo";
+        $user->codigo = Str::uuid()->toString();
+        $user->activation = 1;
+        $user->password = bcrypt("0f1c14l");
+        $user->save();
+
+        $user->assignRole('oficial');
+
+        $official = new Official();
+        $official->nombres = "MILENKA GLADYZ";
+        $official->paterno = "ACARAPI";
+        $official->materno = "PEREZ";
+        $official->user_id = $user->id;
+        $official->save();
 
 
-        $user = new User();        
+        $user = new User();
+        $user->email = "lency.chura@planificacion.gob.bo";
+        $user->codigo = Str::uuid()->toString();
+        $user->activation = 1;
+        $user->password = bcrypt("0f1c14l");
+        $user->save();
+
+        $user->assignRole('oficial');
+
+        $official = new Official();
+        $official->nombres = "LENCY CRISTHIAN";
+        $official->paterno = "CHURA";
+        $official->materno = "CALLISAYA";
+        $official->user_id = $user->id;
+        $official->save();
+
+
+
+
+        $user = new User();
         $user->email = "responsable@sigv3.com";
         $user->codigo = Str::uuid()->toString();
         $user->activation = 1;
-        $user->password = bcrypt("r35p054bl3");        
+        $user->password = bcrypt("r35p054bl3");
         $user->save();
 
         $user->assignRole('responsable');
@@ -70,14 +136,14 @@ class OfficialSeeder extends Seeder
         $official->user_id = $user->id;
         $official->save();
 
-        
-        
 
-        $user = new User();        
+
+
+        $user = new User();
         $user->email = "fiduciario@sigv3.com";
         $user->codigo = Str::uuid()->toString();
         $user->activation = 1;
-        $user->password = bcrypt("f1duc14r10");        
+        $user->password = bcrypt("f1duc14r10");
         $user->save();
 
         $user->assignRole('fiduciario');
